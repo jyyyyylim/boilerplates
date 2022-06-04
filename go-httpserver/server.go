d89c3ea10b2e46c5909c5f8948583ae68a963a00
俊。 <junyaolim0206@gmail.com>
2022-06-04 12:43:49 +0800

    Update server.go

--- go-httpserver/server.go
+++ go-httpserver/server.go
@@ -9,7 +9,7 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
 
-//make a httprequest to localhost port 19232 on route /test
+//rcving httprequest to localhost port 192 on route /test
 func main() {
 	http.HandleFunc("/test", ping)
 	http.ListenAndServe(":192", nil)
