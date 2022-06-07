49a22ad803fafbe1f2cfe321228532d4187ab569
俊。 <junyaolim0206@gmail.com>
2022-06-07 16:32:26 +0800

    Update server.go

--- go-httpserver/server.go
+++ go-httpserver/server.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
 
-//rcving httprequest to localhost port 192 on route /test
+func imgping(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("client " + r.RemoteAddr + " has attemped a connection at: ")
+	fmt.Println(time.Now())
+
+	imgstream, error := ioutil.ReadFile("the funny .png")
+
+	//fmt.Fprintf(w, r)
+	//fmt.Fprintf(w, w)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		//w.Header().Set("Content-Type", "image.png")
+		w.Write(imgstream)
+	}
+
+}
+
+//make a httprequest to localhost port 19232 on route /test
 func main() {
 	http.HandleFunc("/test", ping)
+	http.HandleFunc("/cdn/image", imgping)
+
 	http.ListenAndServe(":192", nil)
 }
